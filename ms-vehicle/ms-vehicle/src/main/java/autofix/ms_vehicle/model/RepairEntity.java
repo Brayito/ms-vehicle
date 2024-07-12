3fb72d80fb545e96de2688bffba919d43aa2b86b
dominiqueOsswald <dominique.osswald@usach.cl>
2024-07-12 01:13:19 -0400

    Conexión entre vehicles y repairs funcionando

---
+++ ms-vehicle/ms-vehicle/src/main/java/autofix/ms_vehicle/model/RepairEntity.java
@@ -0,0 +1,11 @@
+package autofix.ms_vehicle.model;
+
+import lombok.Data;
+
+@Data
+public class RepairEntity {
+
+    private String type;
+    private Integer value;
+    private Integer vehicleId;
+}
--- ms-vehicle/ms-vehicle/src/main/java/autofix/ms_vehicle/repository/VehicleRepository.java
+++ ms-vehicle/ms-vehicle/src/main/java/autofix/ms_vehicle/repository/VehicleRepository.java
@@ -4,6 +4,10 @@ import autofix.ms_vehicle.entity.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VehicleRepository extends JpaRepository <VehicleEntity, Integer> {
+
+
 }
